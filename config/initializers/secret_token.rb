5d098bea3d6753ff0f3022b0555cd3b89656d03d
Henryvw <Henryvw@gmail.com>
2013-11-27 22:15:54 +0100

    initializing

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HvwMain::Application.config.secret_key_base = 'b1322001e52f9cee96e7dade84cbb1a67c99ec4b93c38c22bc2f648087c3ec801341863db3de0da3f77b279241d2f3a4717f63f6946a0d69f55e12e0dea7b185'
